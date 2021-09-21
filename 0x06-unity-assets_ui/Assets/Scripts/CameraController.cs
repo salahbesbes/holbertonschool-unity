cceecf49c74ccbbd730374b2cfa97cb59fa10cd7
salahbesbes <salah_besbes@ymail.com>
2021-09-21 15:34:06 +0100

    updates

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -2,38 +2,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-	private Vector3 offset;
-	public Transform player;
-	public float turnSpeed = 4.0f;
-	private float rotation = 0f;
-	public bool isInverted = false;
-	private void Start()
-	{
+        private Vector3 offset;
+        public Transform player;
+        public float turnSpeed = 4.0f;
+        private float rotation = 0f;
+        public bool isInverted = false;
 
-	}
+        private void Start()
+        {
+                offset = player.position + transform.position;
+        }
 
-	private void LateUpdate()
-	{
-		// update the value of isInverted from the game manager
-		isInverted = GameManager.Instance.IsInverted;
+        private void LateUpdate()
+        {
+                // update the value of isInverted from the game manager
+                isInverted = GameManager.Instance.IsInverted;
 
-		float mouseX = Input.GetAxis("Mouse X") * 100f * Time.deltaTime;
-		float mouseY = Input.GetAxis("Mouse Y") * 100f * Time.deltaTime;
+                // if we update turnSpeed to 400f and replace 100f * 4f it wont work
+                float mouseX = Input.GetAxis("Mouse X") * 100f * turnSpeed * Time.deltaTime;
+                float mouseY = Input.GetAxis("Mouse Y") * 100f * turnSpeed * Time.deltaTime;
 
-		// by rotating the player: the cylinder and the head and the camera
-		// are rotating simyltaly on the X axes
-		player.Rotate(Vector3.up * mouseX);
+                player.Rotate(Vector3.up * mouseX);
 
-		// to rotate only the camera on the Y axes
-		rotation = isInverted ? rotation + mouseY : rotation - mouseY;
+                // to rotate only the camera on the Y axes
+                rotation = isInverted ? rotation + mouseY : rotation - mouseY;
 
+                rotation = Mathf.Clamp(rotation, -90f, 90f);
 
-		rotation = Mathf.Clamp(rotation, -90f, 90f);
-
-		transform.localRotation = Quaternion.Euler(rotation, 0, 0);
-
-		//offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-		//transform.position = player.position + offset;
-		//transform.LookAt(player.position);
-	}
+                transform.localRotation = Quaternion.Euler(rotation, 0, 0);
+        }
 }
--- 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
@@ -2,90 +2,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public CharacterController charCon;
-	public Canvas pauseCanvas;
+        public CharacterController charCon;
+        public Canvas pauseCanvas;
 
-	public float speed = 6f;
-	public float maxJumpHeight = 10f;
-	private bool isGrounded = true;
-	private Vector3 startPosition;
+        public float speed = 6f;
+        public float maxJumpHeight = 10f;
+        private bool isGrounded = true;
+        private Vector3 startPosition;
 
-	// Update is called once per frame
-	private float gravity = -9.8f;
+        // Update is called once per frame
+        private float gravity = -9.8f * 2;
 
-	private Vector3 gravityVelocity = Vector3.zero;
+        private Vector3 gravityVelocity = Vector3.zero;
 
-	//public float jumpVelocity = 0.5f;
+        //public float jumpVelocity = 0.5f;
 
-	private void Start()
-	{
-		// make sure the time scale when the player is rendred is 1
-		Time.timeScale = 1;
-		charCon = GetComponent<CharacterController>();
-		startPosition = transform.position;
-	}
+        private void Start()
+        {
+                // make sure the time scale when the player is rendred is 1
+                Time.timeScale = 1;
+                charCon = GetComponent<CharacterController>();
+                startPosition = transform.position;
+        }
 
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.tag == "DethEnd")
-		{
-			// note this line will not work !! why
-			//transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+        private void OnTriggerEnter(Collider other)
+        {
+                if (other.tag == "DethEnd")
+                {
+                        // note this line will not work !! why
+                        //transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+                }
+        }
 
-		}
-	}
+        private void Update()
+        {
+                if (Input.GetKeyUp(KeyCode.Escape))
+                {
+                        // get the pause menu
+                        PauseMenu pauseMenu = pauseCanvas.GetComponent<PauseMenu>();
 
-	private void FixedUpdate()
-	{
+                        // check if he pause canvas is active and enabled if not
+                        if (pauseCanvas.isActiveAndEnabled == false)
+                        {
+                                // after we pause the game the update methode of the player will not
+                                // run any more so to resume the game the only way is to click on a
+                                // button resume in the canvas not to press the escape buuton again
+                                pauseMenu.Pause();
+                        }
+                }
 
-		if (Input.GetKeyUp(KeyCode.Escape))
-		{
-			// get the pause menu
-			PauseMenu pauseMenu = pauseCanvas.GetComponent<PauseMenu>();
+                isGrounded = charCon.isGrounded;
 
-			// check if he pause canvas is active and enabled if not 
-			if (pauseCanvas.isActiveAndEnabled == false)
-			{
-				// after we pause the game the update methode of the player
-				// will not run any more so to resume the game the only way
-				// is to click on a button resume in the canvas not to press the escape buuton again 
-				pauseMenu.Pause();
-			}
+                if (isGrounded && gravityVelocity.y < 0)
+                {
+                        gravityVelocity.y = -2f;
+                }
 
-		}
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
 
-		isGrounded = charCon.isGrounded;
+                // handle movement
+                Vector3 direction = transform.right * x + transform.forward * z;
+                charCon.Move(direction * speed * Time.deltaTime);
 
-		if (isGrounded && gravityVelocity.y < 0)
-		{
-			gravityVelocity.y = -2f;
-		}
+                //handle jump
+                if (Input.GetButtonDown("Jump") && isGrounded)
+                {
+                        gravityVelocity.y = Mathf.Sqrt(maxJumpHeight * -3.0f * gravity);
+                }
+                //handle gravity
+                gravityVelocity.y += gravity * Time.deltaTime;
+                charCon.Move(gravityVelocity * Time.deltaTime);
 
-
-		float x = Input.GetAxis("Horizontal");
-		float z = Input.GetAxis("Vertical");
-
-		// handle movement
-		Vector3 direction = transform.right * x + transform.forward * z;
-		charCon.Move(direction * speed * Time.fixedDeltaTime);
-
-
-		//handle jump
-		if (Input.GetButtonDown("Jump") && isGrounded)
-		{
-			gravityVelocity.y = Mathf.Sqrt(maxJumpHeight * -3.0f * gravity);
-		}
-		//handle gravity
-		gravityVelocity.y += gravity * Time.fixedDeltaTime;
-		charCon.Move(gravityVelocity * Time.fixedDeltaTime);
-
-
-		// note why this code work properly but does not work in OnTriggerEnter method
-		if (transform.position.y < -30.0f)
-		{
-			transform.position = startPosition + new Vector3(0f, 10f, 0f);
-		}
-
-
-	}
+                // note why this code work properly but does not work in OnTriggerEnter method
+                if (transform.position.y < -30.0f)
+                {
+                        transform.position = startPosition + new Vector3(0f, 10f, 0f);
+                }
+        }
 }
