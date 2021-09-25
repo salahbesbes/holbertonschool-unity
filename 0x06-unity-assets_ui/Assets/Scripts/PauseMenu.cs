5cbee09e8275abd15d1f505be5bfb29d25ad1f12
salahbesbes <salah_besbes@ymail.com>
2021-09-25 15:58:21 +0100

    finish the project

--- 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -4,15 +4,18 @@ using UnityEngine.SceneManagement;
 /// <summary> this component is attached to the pause canvase which is by default deactivated </summary>
 public class PauseMenu : MonoBehaviour
 {
-
-	/// <summary> set the time scale to 0 and activate the Pause canvas ( freeze the game ) </summary>
+	/// <summary>
+	/// set the time scale to 0 and activate the Pause canvas ( freeze the game )
+	/// </summary>
 	public void Pause()
 	{
 		Time.timeScale = 0;
 		this.gameObject.SetActive(true);
 	}
 
-	/// <summary> set the time scale to 1 and disable the Pause canvas ( unfreeze the game ) </summary>
+	/// <summary>
+	/// set the time scale to 1 and disable the Pause canvas ( unfreeze the game )
+	/// </summary>
 	public void Resume()
 	{
 		Time.timeScale = 1;
@@ -29,12 +32,11 @@ public class PauseMenu : MonoBehaviour
 	{
 		Time.timeScale = 1;
 		SceneManager.LoadScene("MainMenu");
-
 	}
+
 	public void Options()
 	{
 		Time.timeScale = 1;
 		SceneManager.LoadScene("Options");
-
 	}
 }
