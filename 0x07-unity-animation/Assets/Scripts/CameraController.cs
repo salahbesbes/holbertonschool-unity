43a1498e681b2b10322d0ac74cbc4f57f5830a5f
salahbesbes <salah_besbes@ymail.com>
2021-10-02 18:11:38 +0100

    animation task

--- 0x07-unity-animation/Assets/Scripts/CameraController.cs
+++ 0x07-unity-animation/Assets/Scripts/CameraController.cs
@@ -5,30 +5,32 @@ public class CameraController : MonoBehaviour
 	private Vector3 offset;
 	public Transform player;
 	public float turnSpeed = 4.0f;
-	private float rotation = 0f;
+	private float rotation, rotationY = 0f;
 	public bool isInverted = false;
 
 	private void Start()
 	{
-		offset = player.position + transform.position;
+		offset = transform.position - player.position;
 	}
 
 	private void LateUpdate()
 	{
 		// update the value of isInverted from the game manager
-		isInverted = GameManager.Instance.IsInverted;
+		//isInverted = GameManager.Instance.IsInverted;
 
 		// if we update turnSpeed to 400f and replace 100f * 4f it wont work
-		float mouseX = Input.GetAxis("Mouse X") * 20f * turnSpeed * Time.deltaTime;
-		float mouseY = Input.GetAxis("Mouse Y") * 20f * turnSpeed * Time.deltaTime;
+		float mouseX = Input.GetAxis("Mouse X") * 70f * turnSpeed * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * 70f * turnSpeed * Time.deltaTime;
 
-		player.Rotate(Vector3.up * mouseX);
+		//player.Rotate(Vector3.up * mouseX);
 
 		// to rotate only the camera on the Y axes
 		rotation = isInverted ? rotation + mouseY : rotation - mouseY;
-
+		rotationY += mouseX;
+		transform.position = player.position + offset;
 		rotation = Mathf.Clamp(rotation, -90f, 90f);
+		rotationY = Mathf.Clamp(rotationY, -90f, 90f);
 
-		transform.localRotation = Quaternion.Euler(rotation, 0, 0);
+		transform.localRotation = Quaternion.Euler(rotation, rotationY, 0);
 	}
 }
