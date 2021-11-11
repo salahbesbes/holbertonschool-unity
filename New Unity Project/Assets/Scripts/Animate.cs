620fa8790f45a36faca6c5fc414f8f67888fbf64
salahbesbes <salah_besbes@ymail.com>
2021-11-11 13:50:08 +0100

    new project

---
+++ New Unity Project/Assets/Scripts/Animate.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+	public Transform cube;
+
+	private void OnMouseUpAsButton()
+	{
+	}
+
+	public void onclickevent()
+	{
+		Debug.Log($"clicked");
+	}
+}
