b47bd04ae1abd692e32f6253a40df9abbb15cc7f
salahbesbes <salah_besbes@ymail.com>
2021-11-11 21:45:52 +0100

    update

---
+++ New Unity Project/Assets/Scripts/OnClickEvent.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class OnClickEvent : MonoBehaviour
+{
+	public void goLinkedIn()
+	{
+		System.Diagnostics.Process.Start("https://www.linkedin.com/in/mohamed-salah-besbes-642106a1/");
+	}
+
+	public void goFaceboob()
+	{
+		System.Diagnostics.Process.Start("https://www.facebook.com/salah.besbes/");
+	}
+
+	public void goMedium()
+	{
+		System.Diagnostics.Process.Start("https://medium.com/me/stories/public");
+	}
+
+	public void goInstagram()
+	{
+		System.Diagnostics.Process.Start("https://www.facebook.com/salah.besbes/");
+	}
+}
